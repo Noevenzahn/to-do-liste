40f0a04134e91ef001830813e188cd7dfe6d41a0
Noevenzahn <info.artuofficial@gmail.com>
2022-02-09 11:34:40 +0100

    add email to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,9 @@
 import LogoutButton from "./LogoutButton";
 
-export default function Nav() {
+export default function Nav({ user }) {
   return (
     <nav>
+      <p className="date">{user.email}</p>
       <LogoutButton />
     </nav>
   );
